81998adf1a19a14ca2d3669d8651a649715b1868
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-09-13 04:40:05 -0500

    Create bwa.sh

---
+++ bwa.sh
@@ -0,0 +1,8 @@
+cd $WORK/lugdunensis_softwares/bwa
+wget https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.17.tar.bz2
+tar -xvjf bwa-0.7.17.tar.bz2
+rm bwa-0.7.17.tar.bz2
+cd bwa-0.7.17
+make
+cd $WORK/lugdunensis
+sbatch bwa.slurm
