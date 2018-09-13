df97ab4b6f86cb9c572fd5ec83e5f64cfeb02206
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-09-13 05:07:21 -0500

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -1,4 +1,4 @@
-cd $WORK/lugdunensis_softwares/bwa
+cd $WORK/lugdunensis-softwares
 wget https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.17.tar.bz2
 tar -xvjf bwa-0.7.17.tar.bz2
 rm bwa-0.7.17.tar.bz2
