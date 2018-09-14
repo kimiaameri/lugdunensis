fa7248bcbe2781069f2440656c165cf2aed520df
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-09-13 20:13:15 -0500

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -4,5 +4,6 @@ tar -xvjf bwa-0.7.17.tar.bz2
 rm bwa-0.7.17.tar.bz2
 cd bwa-0.7.17
 make
-
+cd $HOME/lugdunensis_reference_genome/
+$WORK/lugdunensis-softwares/bwa-0.7.17/bwa Staphylococcus_lugdunensis_N920143.fa
 
