72a2a917f877eea0643e2e2d85d2d0f60b08693e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-09-19 01:07:18 -0500

    Update freebayes.sh

--- freebayes.sh
+++ freebayes.sh
@@ -2,5 +2,7 @@ cd $WORK/lugdunensis-softwares/
 git clone --recursive git://github.com/ekg/freebayes.git
 cd freebayes
 make
+cd $WORK/lugdunensis-softwares/freebayes/vcflib
+make
 cd $WORK/lugdunensis
 sbatch freebayes.slurm
