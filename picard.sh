1f247071005a782052f8cd6ffdca745ba2f64589
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-09-19 23:20:49 -0500

    Update picard.sh

--- picard.sh
+++ picard.sh
@@ -1,1 +1,10 @@
-sbatch picard*.slurm
+sbatch picard1.slurm;
+sbatch picard2.slurm;
+sbatch picard3.slurm;
+sbatch picard4.slurm;
+sbatch picard5.slurm;
+sbatch picard6.slurm;
+sbatch picard7.slurm;
+sbatch picard8.slurm;
+sbatch picard9.slurm;
+sbatch picard10.slurm;
